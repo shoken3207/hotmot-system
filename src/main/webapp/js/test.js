818e0600df3fca6d550d8e896b4afd56fc1d2c54
shoken <shoken4797@gmail.com>
2023-12-19 14:57:47 +0900

    リクエストのjsonをモデル化

--- src/main/webapp/js/test.js
+++ src/main/webapp/js/test.js
@@ -6,7 +6,30 @@ import {fetchProductsByCategory} from '../js/master.js'
 window.addEventListener("DOMContentLoaded", async () => {
 	console.log("called")
 	await fetchProductsByCategory(5)
-	await fetch('/hotmot/test')
+	const options = [
+		{
+			cartId: 1,
+			productId: 3,
+			riceId: 4,
+			quantity: 2
+		},
+		{
+			cartId: 10,
+			productId: 32,
+			riceId: 2,
+			quantity: 20
+		},
+		{
+			cartId: 2,
+			productId: 9,
+			riceId: 2,
+			quantity: 7
+		}
+	]
+	await fetch('/hotmot/test', {
+		method: "POST",
+		body: JSON.stringify(options)
+	})
         .then(response => {
 			console.log("res: ", response)
             if (!response.ok) {
